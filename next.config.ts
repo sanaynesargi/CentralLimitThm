95637cda5ff638eb1604e0bc2a368d2b32749667
Sanay Nesargi <sanaynesargi@gmail.com>
2026-01-26 15:38:31 -0600

    Add complete codebase including components and all project files

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/CentralLimitThm",
+
+  experimental: {
+    optimizePackageImports: ["@chakra-ui/react"],
+  },
 };
 
 export default nextConfig;
